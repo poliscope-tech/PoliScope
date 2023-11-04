34600988a2a28d1ad81e4fec28310f8fe3501c26
Clovis VT <>
2023-11-04 14:52:51 -0700

    update scraper

--- scraper/legistar_scraper.py
+++ scraper/legistar_scraper.py
@@ -1,13 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 
-class LegistarScraper:
+class MyWebScraper:
     def __init__(self, url):
         self.url = url
 
     def scrape(self):
         response = requests.get(self.url)
+        if response.status_code != 200:
+            raise Exception(f"Failed to retrieve page with status code: {response.status_code}")
+
         soup = BeautifulSoup(response.text, 'html.parser')
-        # Scrape title, date, vote
-        # This is a placeholder, update with actual scraping logic
-        return {"title": "", "date": "", "vote": ""}
+
+        # Find the element with the desired ID
+        starting_element = soup.find(id='ctl00$ContentPlaceHolder1$gridDepartments$ctl00$ctl02$ctl01$ctl00')
+        breakpoint()
+
+        # Navigate to its parent element
+        tbody = starting_element
+
+        # Loop over all tr elements that are children of tbody
+        for row in tbody.find_all('tr', recursive=False):
+            columns = row.find_all('td')
+            field_texts = [col.text.strip() for col in columns]
+            print(' | '.join(field_texts))
+
+# Usage:
+url = "https://sfgov.legistar.com/PersonDetail.aspx?ID=196476&GUID=63B530EB-D641-42BB-BFEB-A1367DC844CE&Search="
+scraper = MyWebScraper(url)
+scraper.scrape()
+
+ 
