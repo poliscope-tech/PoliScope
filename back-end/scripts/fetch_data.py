e38ea0130f5d191aa06b43a0e86dd7a2b7f4be6f
tanzeelak <tnkhan8042@gmail.com>
2023-11-04 16:41:57 -0700

    aggregated politicians and now fetching all data from one data

--- back-end/scripts/fetch_data.py
+++ back-end/scripts/fetch_data.py
@@ -5,5 +5,5 @@ url: str = "https://hxrggsnimtifedjvpupp.supabase.co"
 key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4cmdnc25pbXRpZmVkanZwdXBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkxMzU5NDYsImV4cCI6MjAxNDcxMTk0Nn0.eAZYkghBmpIVkED5QWvjcMyFvsfcpgiyFSNADV3AGEA"
 supabase: Client = create_client(url, key)
 
-response = supabase.table('aaron_peskin').select("*").execute()
+response = supabase.table('politicians').select("*").execute()
 print(response.data)
