b0b2ee3edcdb6209983f224775d6a5fc4f768b72
Jan Koeppen <jan@yaneq.com>
2023-11-05 15:05:12 -0800

    merge

---
+++ front-end/src/components/Avatars.tsx
@@ -0,0 +1,26 @@
+export const Avatars = () => {
+  return (
+    <div className="absolute right-0 top-0 mr-5 mt-10 flex space-x-4">
+      <img
+        src="/images/health.png" // updated image path for health
+        alt="Healthcare Avatar"
+        className="h-16 w-16 rounded-full border-2 border-white object-contain shadow-lg"
+      />
+      <img
+        src="/images/educ.png" // updated image path for education
+        alt="Education Avatar"
+        className="h-16 w-16 rounded-full border-2 border-white object-contain shadow-lg"
+      />
+      <img
+        src="/images/house.png" // updated image path for housing
+        alt="Housing Avatar"
+        className="h-16 w-16 rounded-full border-2 border-white object-contain shadow-lg"
+      />
+      <img
+        src="/images/envir.png" // updated image path for environment
+        alt="Environment Avatar"
+        className="h-16 w-16 rounded-full border-2 border-white object-contain shadow-lg"
+      />
+    </div>
+  )
+}
