32426ee51ce06395e28515b0a1798f9a909a3dd2
William-Misiaszek <wmisiasz@gmail.com>
2023-11-05 14:54:58 -0800

    Dean Preston

--- front-end/src/components/Intro.tsx
+++ front-end/src/components/Intro.tsx
@@ -4,14 +4,14 @@ export function Intro() {
       <h1 className="tight mt-14 font-display text-4xl font-light text-white">
         <span className="text-sky-300">Board of Supervisors</span>
         <br />
-        Matt Dorsey <br />
+        Dean Preston <br />
       </h1>
       <p className="mt-4 text-sm/6 text-gray-300">
-        Supervisor Matt Dorsey, representing San Francisco's District 6,
-        advocates for the Affordable Homes Now amendment to expedite affordable
-        and mixed-income housing. A member of the substance use recovery
-        community, he promotes recovery paths for addiction and public safety
-        investments.
+        Supervisor Dean Preston of SF's District 5 is a Democratic Socialist and
+        tenant rights attorney. With 20 years in the affordable housing
+        movement, he fought for rent control, formed a tenant rights group, and
+        authored a law for tenant legal representation. He champions community
+        engagement, supports small businesses, and seeks systemic change.
       </p>
       {/* Removed Sign Up Form and Icon Links */}
     </>
