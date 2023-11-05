b17b02f5766543aea767e4e7c60a23564f092b2a
William-Misiaszek <wmisiasz@gmail.com>
2023-11-05 11:06:54 -0800

    V2. Sticky Graph WIP

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -10,32 +10,24 @@ import clsx from 'clsx'
 Chart.register(CategoryScale)
 Chart.register(...registerables)
 
-
 function BarWrapper({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<'div'>) {
-  return (
-    <div 
-    style={{ marginLeft: '50%', position: "sticky", bottom: "0"}}
-    {...props} />
-  )
-}
-
-
-const BarChart = ({ result }: { result: any }) => {
   const data = {
     labels: ['Housing', 'Tenant', 'Homelessness', 'Faster Permits', 'Zoning'],
     datasets: [
       {
         label: 'Score',
-        data: [
-          result['affordable_housing_development_score'],
-          result['tenant_protections_score'],
-          result['homelessness_and_supportive_housing_score'],
-          result['faster_permitting_process_and_bureaucracy_score'],
-          result['land_use_and_zoning_reform'],
-        ],
+        data: props.result
+          ? [
+              props.result['affordable_housing_development_score'],
+              props.result['tenant_protections_score'],
+              props.result['homelessness_and_supportive_housing_score'],
+              props.result['faster_permitting_process_and_bureaucracy_score'],
+              props.result['land_use_and_zoning_reform'],
+            ]
+          : [],
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
@@ -56,9 +48,12 @@ const BarChart = ({ result }: { result: any }) => {
       },
     ],
   }
+
   return (
-    <div className="max-h-72">
-      <h2>Bar Example (custom size)</h2>
+    <div
+      style={{ marginLeft: '50%', position: 'sticky', bottom: '0' }}
+      {...props}
+    >
       <Bar
         data={data}
         width={400}
@@ -72,20 +67,19 @@ const BarChart = ({ result }: { result: any }) => {
 }
 
 export default function FeedPage() {
+  // Assume the first result is what we want to show in the sticky graph.
+  const stickyResult = llmResults[0] // or another logic to determine which result to show
+
   return (
     <Layout>
+      <BarWrapper result={stickyResult} />
       {llmResults.map((result: any) => (
-        <div>
-          <Article id={result} date={new Date(result.actionDate)} key={result.id}>
-            <div>
-              <h2>{result.title}</h2>
-              <p>{result.meetingBody}</p>
-            </div>            
-          </Article>
-          <BarWrapper>
-              <BarChart result={result} />
-          </BarWrapper>
-        </div>
+        <Article id={result} date={new Date(result.actionDate)} key={result.id}>
+          <div>
+            <h2>{result.title}</h2>
+            <p>{result.meetingBody}</p>
+          </div>
+        </Article>
       ))}
     </Layout>
   )
