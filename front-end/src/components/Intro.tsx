8de1a7c47c375cfe9a8b9626683f5369144de10d
Jan Koeppen <jan@yaneq.com>
2023-11-04 22:39:10 -0700

    cleanups

--- front-end/src/components/Intro.tsx
+++ front-end/src/components/Intro.tsx
@@ -65,21 +65,7 @@ export function Intro() {
         <IconLink href="#" icon={GitHubIcon} className="flex-none">
           GitHub
         </IconLink>
-        <IconLink href="/feed.xml" icon={FeedIcon} className="flex-none">
-          RSS
-        </IconLink>
       </div>
     </>
-  )
-}
-
-export function IntroFooter() {
-  return (
-    <p className="flex items-baseline gap-x-2 text-[0.8125rem]/6 text-gray-500">
-      Brought to you by{' '}
-      <IconLink href="#" icon={TwitterIcon} compact large>
-        Joe Davola
-      </IconLink>
-    </p>
   )
 }
