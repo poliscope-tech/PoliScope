038fd4563a8d4d9b3cab628e71f56162f0d2635c
William-Misiaszek <wmisiasz@gmail.com>
2023-11-14 10:19:25 -0800

    No categories yet.

--- front-end/src/components/Avatars.tsx
+++ front-end/src/components/Avatars.tsx
@@ -1,7 +1,7 @@
 export const Avatars = () => {
   return (
     <div className="absolute right-0 top-0 mr-5 mt-10 flex space-x-4">
-      <img
+      {/* <img
         src="/images/health.png" // updated image path for health
         alt="Healthcare Avatar"
         className="h-16 w-16 rounded-full border-2 border-white object-contain shadow-lg"
@@ -20,7 +20,7 @@ export const Avatars = () => {
         src="/images/envir.png" // updated image path for environment
         alt="Environment Avatar"
         className="h-16 w-16 rounded-full border-2 border-white object-contain shadow-lg"
-      />
+      /> */}
     </div>
   )
 }
