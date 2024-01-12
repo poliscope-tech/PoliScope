66d2dd34fcd97320a80c13e71f4bcbcb8b607d65
William-Misiaszek <wmisiasz@gmail.com>
2024-01-11 22:27:17 -0800

    hamburger type update

--- front-end/src/components/HamburgerIcon.tsx
+++ front-end/src/components/HamburgerIcon.tsx
@@ -1,15 +1,19 @@
-
 // HamburgerIcon.tsx
 
 import React from 'react';
 
+// Include the isOpen prop in the type
 type HamburgerIconProps = {
     onClick: () => void;
+    isOpen: boolean;  // Add this line
 };
 
-export const HamburgerIcon: React.FC<HamburgerIconProps> = ({ onClick }) => {
+export const HamburgerIcon: React.FC<HamburgerIconProps> = ({ onClick, isOpen }) => {
+    // Apply classes conditionally based on isOpen
+    const iconClass = isOpen ? 'hamburger-icon open' : 'hamburger-icon';
+
     return (
-        <div className="hamburger-icon" onClick={onClick}>
+        <div className={iconClass} onClick={onClick}>
             ☰
         </div>
     );
